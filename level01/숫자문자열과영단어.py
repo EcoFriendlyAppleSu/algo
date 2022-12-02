1df904998d8943dbd633308c480fc49baad70f6d
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2022-12-02 15:54:46 +0900

    12/02 algo

---
+++ level01/숫자문자열과영단어.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+
+def solution(s):
+    board = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    boardDict = {}
+
+    for index in range(len(board)):
+        boardDict[board[index]] = str(index)
+
+    for element in board:
+        if element in s:
+            s = str.replace(s,element, boardDict[element])
+    return int(s)
+
+if __name__ == '__main__':
+    print(solution("one4seveneight"))
+    print(solution("23four5six7"))
+    print(solution("2three45sixseven"))
+    print(solution("123"))
+
+
+# O(n) -> 10 + 10 * (n * (1 + m2/m1)) -> 10n + 10*(m2/m1) + 10
+# initialize data structures.     # max time O(n)
+# while find next match:          # max time O(n*m1)
+#     copy unchanged string.      # max time O(n)
+#     copy replacement            # max time O((n/m1) * m2) + O(n)
+# copy rest of the string         # max time O(n)
+# 시간 복잡도 : O(n * (1 + m2/m1))
