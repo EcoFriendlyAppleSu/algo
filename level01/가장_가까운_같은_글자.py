791e894574b20666ae2a4bc32d0fbfd4f71fe34d
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2022-12-12 16:46:55 +0900

    12/12 algo

--- level01/가장_가까운_같은_글자.py
+++ level01/가장_가까운_같은_글자.py
@@ -2,8 +2,8 @@
 enumerate 존재 기억하기
 '''
 def solution(s):
-    marked = {}
-    answer = []
+    marked = {} # dictionary
+    answer = [] # answer
     for index, letter in enumerate(s):
         answer.append(index - marked[letter] if letter in marked else -1)
         marked[letter] = index
---
+++ level02/뉴스_클러스터링.py
@@ -0,0 +1,77 @@
+import math
+def getTwoLengthSizeAry(s):
+    result = []
+
+    # 1000
+    for i in range(len(s)):
+        temp = s[i:i+2]
+        if str(temp).isalpha() == False:
+            continue
+        if len(temp) != 2:
+            continue
+        result.append(temp)
+    return result
+
+def solution(str1, str2):
+    answer = 0
+    intersaction = {}
+    union = {}
+
+    # 1000
+    str1 = str(str1).lower()
+    str2 = str(str2).lower()
+
+    print(str1, str2)
+
+    # 1000
+    str1Ary = getTwoLengthSizeAry(str1)
+    str2Ary = getTwoLengthSizeAry(str2)
+
+    print("집합 = ", str1Ary, str2Ary)
+
+    # 1000
+    setStr1 = set(str1Ary)
+    # 1000
+    setStr2 = set(str2Ary)
+
+    print("교집합 = ", setStr1 & setStr2)
+
+    # 999
+    for index in setStr1 & setStr2:
+        count1 = str1Ary.count(index)
+        count2 = str2Ary.count(index)
+        if count1 >= count2:
+            intersaction[index] = count2
+        else:
+            intersaction[index] = count1
+
+    print("j 교집합 = ",intersaction)
+
+    print("합집합 = ", setStr1 | setStr2)
+
+    # 999 * 2 = 2000
+    for index in setStr1 | setStr2:
+        count1 = str1Ary.count(index)
+        count2 = str2Ary.count(index)
+        if count1 >= count2:
+            union[index] = count1
+        else:
+            union[index] = count2
+
+    print("j 합집합 = ", union)
+
+    print("sum of intersaction = ", sum(intersaction.values()))
+    print("sum of union = ", sum(union.values()))
+
+    try:
+        answer = float(sum(intersaction.values()) / sum(union.values()))
+    except ZeroDivisionError:
+        answer = 1
+
+    print("hfklsdjflasdkj = ", int(3.6))
+    return math.trunc(answer * 65536)
+
+# print(solution("E=M*C^2", "e=m*c^2"))
+solution("FRANCE", "french")
+# solution("aa1+aa2", "AAAA12")
+# solution({1,1,2,2,3},  {1, 2, 2, 4, 5})
