eef0f395adb96fbc94bdde5f5f9992f61598a254
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2022-12-13 16:30:41 +0900

    12/13 algo

---
+++ level02/k진수에서_소수_개수_구하기.py
@@ -0,0 +1,91 @@
+import math
+
+def makeNumberToJinsu(n, k):
+    jinsu = []
+    if k == 10:
+        return str(n)
+
+    # O(1) = 333333
+    while n != 0:
+        jinsu.append(n%k)
+        n //= k
+    jinsu.reverse()
+    jinsu = list(map(str, jinsu))
+    return "".join(jinsu)
+
+def isPrime(number):
+    # O(1) = 577
+    for i in range(2, int(math.sqrt(number)) + 1):
+        if number % i == 0:
+            return False # not prime number
+    return True # prime number
+
+def judgeNumberIsPrime(jinsu):
+    result = []
+
+    if '' in jinsu:
+        return result
+    jinsu = list(map(int, jinsu))
+    print("prime jinsu = ", jinsu)
+
+    # O(n) = 333333
+    for i in jinsu:
+        if i == 1:
+            continue
+        if not isPrime(i):
+            continue
+        result.append(i)
+    print("prime number list = ", result)
+    return result
+
+
+def judgeProblemPrimeNumber(givenNumber, primeNumbers):
+    result = []
+    primeNumbers = list(map(str, primeNumbers))
+    print("givenNumber = ", givenNumber)
+    print("primeNumbers = ", primeNumbers)
+
+    # O(n) = 101
+    for element in primeNumbers:
+        if "0" + element + "0" in givenNumber:
+            result.append(element)
+            continue
+        if element + "0" in givenNumber:
+            result.append(element)
+            continue
+        if "0" + element in givenNumber:
+            result.append(element)
+            continue
+        if element == givenNumber:
+            result.append(element)
+            continue
+
+    return result
+
+'''
+1 ≤ n ≤ 1,000,000
+3 ≤ k ≤ 10
+'''
+def solution(n, k):
+    answer = -1
+
+    jinsu = makeNumberToJinsu(n, k)
+    print("jinsu = ", jinsu)
+    compareString = jinsu
+
+    jinsu = jinsu.split("0") # remove zero
+
+    print("jinsu 1 = ", jinsu)
+
+    primeNumbers = judgeNumberIsPrime(jinsu)
+
+    result = judgeProblemPrimeNumber(compareString, primeNumbers)
+    answer = len(result)
+    return answer
+
+# print(solution(143,3))
+# print(solution(437674, 3))
+# print(solution(110011, 10))
+# print(solution(1000000, 3))
+print(solution(100000000000, 10))
+
